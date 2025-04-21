bca68bb777235f42c773c149e5e6983188ce5e5b
Daniel B <Dragon863.dev@gmail.com>
2025-04-21 12:15:26 +0100

    Fix notifications

--- src/prompt_task.py
+++ src/prompt_task.py
@@ -28,7 +28,7 @@ def sendNotification(
     title: str = "Notification",
     ttl: int = 60 * 10,
     filters: list = [],
-    channel: str = os.getenv("ONESIGNAL_GENERIC_CHANNEL"),
+    # channel: str = os.getenv("ONESIGNAL_GENERIC_CHANNEL"),
     priority: int = 10,
     small_icon="ic_stat_onesignal_default",
 ):
@@ -45,7 +45,7 @@ def sendNotification(
         ttl=ttl,
         headings={"en": title},
         filters=filters,
-        android_channel_id=channel,
+        # android_channel_id=channel,
         android_accent_color="737EFF",
         is_android=True,
         is_ios=True,
